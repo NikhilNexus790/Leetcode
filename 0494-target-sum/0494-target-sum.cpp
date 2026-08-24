6a59f8f21bb8dc6e952ddd1e93190f39cd2161c3
NikhilNexus790 <nikhilpd790@gmail.com>
2026-08-25 00:28:08 +0530

    Time: 3 ms (85.94%), Space: 15.6 MB (45.32%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,28 +1,40 @@
 class Solution {
 public:
 
-    int TargetSum(vector<int>& nums, int sum,
-                  vector<vector<int>>& dp, int n) {
+    int TargetSum(vector<int>& nums, int ind, int target,
+                  vector<vector<int>>& dp) {
 
-        if (n == 0) {
-            if (sum == 0)
+        // Base case
+        if (ind == 0) {
+
+            // nums[0] = 0 and target = 0
+            // Two possibilities: +0 and -0
+            if (target == 0 && nums[0] == 0)
+                return 2;
+
+            // Either pick nums[0] or don't pick it
+            if (target == 0 || target == nums[0])
                 return 1;
+
             return 0;
         }
 
-        if (dp[n][sum] != -1)
-            return dp[n][sum];
+        // Already calculated
+        if (dp[ind][target] != -1)
+            return dp[ind][target];
 
-        if (nums[n - 1] > sum) {
-            return dp[n][sum] =
-                TargetSum(nums, sum, dp, n - 1);
+        // Don't pick nums[ind]
+        int notTake = TargetSum(nums, ind - 1, target, dp);
+
+        int take = 0;
+
+        // Pick nums[ind] only if it doesn't exceed target
+        if (nums[ind] <= target) {
+            take = TargetSum(nums, ind - 1,
+                             target - nums[ind], dp);
         }
 
-        int notTake = TargetSum(nums, sum, dp, n - 1);
-
-        int take = TargetSum(nums, sum - nums[n - 1], dp, n - 1);
-
-        return dp[n][sum] = notTake + take;
+        return dp[ind][target] = take + notTake;
     }
 
 
@@ -30,19 +42,23 @@ public:
 
         int n = nums.size();
 
-        int TotalSum = accumulate(nums.begin(), nums.end(), 0);
+        int totalSum = accumulate(nums.begin(), nums.end(), 0);
 
-        if (target + TotalSum < 0 ||
-            (target + TotalSum) % 2 != 0)
+        // Impossible cases
+        if (totalSum + target < 0)
             return 0;
 
-        int sum1 = (target + TotalSum) / 2;
+        if ((totalSum + target) % 2 != 0)
+            return 0;
+
+        // Convert Target Sum into Subset Sum
+        int subsetSum = (totalSum + target) / 2;
 
         vector<vector<int>> dp(
-            n + 1,
-            vector<int>(sum1 + 1, -1)
+            n,
+            vector<int>(subsetSum + 1, -1)
         );
 
-        return TargetSum(nums, sum1, dp, n);
+        return TargetSum(nums, n - 1, subsetSum, dp);
     }
 };
